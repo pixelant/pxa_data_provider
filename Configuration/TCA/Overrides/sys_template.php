bc73a0eaacb7ea97d8d45bcb48c9f5485b6565b3
mabolek <mathias@lilio.com>
2020-01-24 13:08:41 +0100

    [TASK] Added static templates

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,13 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'pxa_data_provider',
+    'Configuration/TypoScript/Extbase',
+    'Defaults for Extbase projects'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'pxa_data_provider',
+    'Configuration/TypoScript/PxaProductManager',
+    'Defaults for pxa_product_manager'
+);
