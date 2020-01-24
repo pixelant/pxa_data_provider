e19a869c0d1cbbae0f883c01d4ba87d799c2ea64
mabolek <mathias@lilio.com>
2020-01-24 13:26:50 +0100

    [TASK] Started adding tests

---
+++ Tests/Pixelant/PxaDataProvider/Domain/DataProvider/ConfigurableDataProviderTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Unit\Domain\DataProvider;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use Pixelant\PxaDataProvider\Domain\DataProvider\ConfigurableDataProvider;
+
+class ConfigurableDataProviderTest extends UnitTestCase
+{
+
+    public function testDataForObject()
+    {
+
+    }
+
+    public function testDataForObjects()
+    {
+
+    }
+}
